a104e5053fad56486a32bd8ae5e8b6f71b0300c3
GajananT <gajanant@emids.com>
2024-04-02 11:50:21 +0530

    Addes cors

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const errorHandler = require("./middleware/errorHandler");
+const cors = require("cors");
 const dotenv = require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
-
+app.use(cors());
 app.use(express.json());
 //app.use("/api", );
 app.use("/api", [require("./routes/allocationRoutes")]);
